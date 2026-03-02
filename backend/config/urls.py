fd3ebfd19408b89ddc08f8712bdaa1b3e39ca708
jithin <jithinjzx@gmail.com>
2026-03-02 21:55:51 +0530

    debug: add health check at root

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 urlpatterns = [
+    path("", lambda r: HttpResponse("System is online! Mode: " + ("DEBUG" if "DEBUG" else "PRODUCTION"))),
     path("admin/", admin.site.urls),
     path("auth/", include("users.urls")),
     path("api/", include("users.api_urls")),
